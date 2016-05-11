f2c31f52075cdf22302b16efddec39ef07fb8265
destino92 <destinodello@gmail.com>
2016-05-12 02:19:34 +0300

    minified html and js to dist/

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,34 @@
 var gulp = require('gulp');
 
 // include plug-ins
-var jshint = require('gulp-jshint');
+var jshint = require('gulp-jshint'),
+	htmlmin = require('gulp-htmlmin'),
+	concat = require('gulp-concat'),
+	stripDebug = require('gulp-strip-debug'),
+	uglify = require('gulp-uglify');
 
 // JS hint task
 gulp.task('jshint', function(){
-	gulp.src('js/*.js')
+	return gulp.src('js/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
+});
+
+// htmlmin tastk
+gulp.task('htmlmin', function(){
+	return gulp.src('*.html')
+		.pipe(htmlmin({collapseWhitespace: true,
+						removeComments: true,
+						minifyJS: true
+					}))
+    	.pipe(gulp.dest('dist'))
+});
+
+// JS concat, strip debugging and minify
+gulp.task('scripts', function() {
+  gulp.src('js/*.js')
+    .pipe(concat('app.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'));
 });
