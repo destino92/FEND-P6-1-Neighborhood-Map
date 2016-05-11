c91b359675ee51c93afd038e7a1fe6672b21ac70
destino92 <destinodello@gmail.com>
2016-05-11 23:56:53 +0300

    commented app.js

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ var NeighborhoodMap = function(){
 			url: url,
 			dataType: 'jsonp',
 			success: function(response){
-				//remove console.log later
+				// Call callback with response as argument
 				callback(response);
 			},
 			error: function(xhr, ajaxOptions, thrownError){
@@ -30,17 +30,17 @@ var NeighborhoodMap = function(){
 	}
 
 
-	/* puhsPlaces is a callback to be used inside getPlaces ajax
+	/* setPlacesAndMarker is a callback to be used inside getPlaces ajax
 	 * success calls.
-	 * it creates an abject based on the data retrieved from ajax 
+	 * It creates an object based on the data retrieved from ajax 
 	 * and pushes it to the places array.
 	 */
-	function pushPlaces(result){
+	function setPlacesAndMarker(result){
 		// Get all the items from the ajax response and store it 
 		// to locations.
-		var locations = result.response.groups[0].items;
-		var bounds = new google.maps.LatLngBounds(); 
-		var fourSquareUrl = 'https://foursquare.com/v/';
+		var locations = result.response.groups[0].items,
+			   bounds = new google.maps.LatLngBounds(), 
+		       fourSquareUrl = 'https://foursquare.com/v/';
 
 		// Loop trough locations variable
 		locations.forEach(function(location, i){
@@ -65,11 +65,15 @@ var NeighborhoodMap = function(){
 						})
 			})
 
+			// Extend map bound with the current place marker
 			bounds.extend(places()[i].marker.getPosition());
 		});
 
+		// Set map to fit bounds
 		map.fitBounds(bounds);
 
+		// Add an invest listener for window resize so that the map can be responsive
+		// And all markers are still visible regardless of the screen size
         google.maps.event.addDomListener(window, 'resize', function() {
         	var center = map.getCenter();
         	google.maps.event.trigger(map, "resize");
@@ -77,35 +81,50 @@ var NeighborhoodMap = function(){
         	map.fitBounds(bounds);
    		});
 
-		// Loop into each place and set an click event listner to it's marker
+		// Loop into each place and set a click event listner to it's marker
+		// to activate marker animation and open infowindow if marker is clicked
 		ko.utils.arrayForEach(places(), function(place){
 			google.maps.event.addListener(place.marker, 'click', function() {
+				// If there is a bouncing marker and the bouncing marker
+				// Is not the current marker set animation for the bouncing marker
+				// to be null
 				if(bouncingMarker && bouncingMarker !== place.marker){
 					bouncingMarker.setAnimation(null);
 				}
 
+				// Close infowindow that is open
 				if(infoWindow)infoWindow.close();
 
-				$('ul').find('li').removeClass('selected');
-               	$('ul').find('li:contains('+ place.name + ')').addClass('selected');
+				// Set the chosen marker place to be chosenPlace
+				// for styling in the place list
+				chosenPlace(place);
 
+				// Set animation of the current marker
                 place.marker.setAnimation(google.maps.Animation.BOUNCE);
+
+                // Set the bouncingMarker to the current marker
                 bouncingMarker = place.marker;
+
+                // Set infowindow to contain information of the current marker
+                // place.
                 infoWindow = new google.maps.InfoWindow({
                     content: '<div><h1>' + place.name + '</h1><p class="tip">' + place.tip + '</p><ul><li><span class="bold-text">RATING:</span> '
                     		+ place.rating + '</li><li><span class="bold-text">ADDRESS:</span> ' + place.address + 
                     		'</li><li><span class="bold-text">TEL:</span> ' + place.phone +
                     		'</li><li><a href="' + place.url + '" target="_blank">View on Foursquare</li></div></ul>'
                 });
+
                 infoWindow.open(map, place.marker);
+
+                // Center the map to the current marker
                 map.setCenter(place.marker.getPosition());
                 map.setZoom(16);
 			})
 		})
 	}
 
-	// Call getPlaces with pushPlaces as an argument.
-	getPlaces(pushPlaces);
+	// Call getPlaces with setPlacesAndMarker as an argument.
+	getPlaces(setPlacesAndMarker);
 
 	// Function to initialize this module.
 	var init = function(){
@@ -118,17 +137,29 @@ var NeighborhoodMap = function(){
 	/* After all initialization has been done i can now place the code to update the DOM
 	 * Below */
 
-	// Click event to be trigered when a map marker or title in the sidebar list is clicked
+	// Click event to be trigered when a place in the sidebar list is clicked
 	function placeClick(place){
+		// set the clicked place to be chosenPlace for styling
+		// not that i call this twice to avoid issue when
+		// the first item clicked is a place and not a marker
 		chosenPlace(place);
+
+		// Trigger the click event of the place marker
 		google.maps.event.trigger(chosenPlace().marker, 'click');
 	}
 
+	// Define a computed observale to handle search activity
+	// from search input
 	searched = ko.computed(function(){
+		// Here visible and not visible are arrays that will contains
+		// places and marker that are visible and not visible
 		var notVisible = [],
 			visible    = [];
 
+		// Filter places accordind to the search input value
 		return ko.utils.arrayFilter(places(), function(place){
+			// If the current place doesn't match the search input value
+			//remove it's marker from the map and the list
 			if(place.name.toLowerCase().indexOf(query().toLowerCase()) < 0){
 				notVisible.push(place);
 				notVisible.forEach(function(place){
@@ -141,6 +172,9 @@ var NeighborhoodMap = function(){
 					place.marker.setMap(map);
 				})
 
+				// If the current place matches the search input value
+				// set it's marker to be visible on the map and on the list or
+				// if there is no search input show all markers and places
 				return ((query().length == 0 || place.name.toLowerCase().indexOf(query().toLowerCase()) > -1 ))
 			}
 		})
