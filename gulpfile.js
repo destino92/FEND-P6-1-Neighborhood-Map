816e3b6d7e7c37bde9dda12a4cfb734b40e4244e
destino92 <destinodello@gmail.com>
2016-05-12 16:40:15 +0300

    organised folders and added browsync module

--- gulpfile.js
+++ gulpfile.js
@@ -8,57 +8,61 @@ var jshint = require('gulp-jshint'),
 	stripDebug = require('gulp-strip-debug'),
 	uglify = require('gulp-uglify'),
 	autoprefix = require('gulp-autoprefixer'),
-	cleanCSS = require('gulp-clean-css');
+	cleanCSS = require('gulp-clean-css'),
+  browserSync = require('browser-sync').create();
+
+// broserSync task
+gulp.task('browserSync', function(){
+  return browserSync.init({
+    server: {
+      baseDir: 'src'
+    }
+  });
+});
 
 // JS hint task
 gulp.task('jshint', function(){
-	return gulp.src('js/*.js')
+	return gulp.src('src/js/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
 
 // htmlmin tastk
 gulp.task('htmlmin', function(){
-	return gulp.src('*.html')
+	return gulp.src('src/*.html')
 		.pipe(htmlmin({collapseWhitespace: true,
 						removeComments: true,
 						minifyJS: true
 					}))
     	.pipe(gulp.dest('dist'))
+      .pipe(browserSync.reload({stream:true}));
 });
 
 // JS concat, strip debugging and minify
 gulp.task('scripts', ['jshint'], function() {
-  	return gulp.src('js/*.js')
-    	.pipe(concat('app.js'))
-    	.pipe(stripDebug())
-    	.pipe(uglify())
-    	.pipe(gulp.dest('dist/js/'));
+  return gulp.src('src/js/*.js')
+    .pipe(concat('app.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 // CSS concat, auto-prefix and minify
 gulp.task('styles', function() {
-  	return gulp.src(['css/*.css'])
-    	.pipe(concat('style.css'))
-    	.pipe(autoprefix('last 2 versions'))
-    	.pipe(cleanCSS())
-    	.pipe(gulp.dest('dist/css/'));
+  return gulp.src(['src/css/*.css'])
+    .pipe(concat('style.css'))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.reload({stream:true}));
 });
+
+gulp.task('watch', function(){
+  gulp.watch(['htmlmin']);
+  gulp.watch('src/js/*.js',['scripts']);
+  gulp.watch('src/css/*.css', ['styles']);
+})
 
 // default gulp task
-gulp.task('default', ['htmlmin', 'scripts', 'styles'], function() {
-  // watch for HTML changes
-  gulp.watch('*.html', function() {
-    gulp.run('htmlmin');
-  });
-
-  // watch for JS changes
-  gulp.watch('js/*.js', function() {
-    gulp.run('scripts');
-  });
-
-  // watch for CSS changes
-  gulp.watch('css/*.css', function() {
-    gulp.run('styles');
-  });
-});
+gulp.task('default', ['browserSync', 'watch']);
